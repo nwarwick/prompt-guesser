806784f6e0afb5ced67635153fae494180e5fce0
Nick Warwick <nicholaswarwick@icloud.com>
2025-02-01 08:43:55 -0500

    Update to Rails 8 and Ruby 3.4.1

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -9,13 +9,14 @@ class GuessesController < ApplicationController
       calculate_score(@guess.text)
       render :score
     else
-      render :new, status: :unprocessable_entity, notice: "At least try to guess something"
+      render :new, status: :unprocessable_entity, notice: 'At least try to guess something'
     end
   end
 
   def calculate_score(guess_text)
-    # TODO:
-    @score = guess_text.similar($prompt).round
+    max_length = [$prompt.length, guess_text.length].max
+    distance = DidYouMean::Levenshtein.distance(guess_text.downcase, $prompt.downcase)
+    @score = ((max_length - distance.to_f) / max_length * 100).round
   end
 
   private
